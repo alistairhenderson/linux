4728930a627dbae032a6cfdd7384063af47728c5
Alistair Henderson <al@alistairhenderson.com>
2023-01-05 16:52:47 +0000

    Update rename-hostname.sh

--- rename-hostname.sh
+++ rename-hostname.sh
@@ -4,6 +4,6 @@ tee /etc/hostname <<EOF
 ${hostname} 
 EOF
 
-echo "#deb [check-date=no] file:///cdrom focal main restricted" >> /etc/apt/sources.list
+echo "#deb [check-date=no] file:///cdrom focal main restricted" > /etc/apt/sources.list
 
 reboot
