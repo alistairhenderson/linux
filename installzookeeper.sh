3f82ac313bfe7e75338cd7c94e0826522cec8c64
Alistair Henderson <al@alistairhenderson.com>
2023-01-05 15:24:36 +0000

    Create installzookeeper.sh

---
+++ installzookeeper.sh
@@ -0,0 +1,36 @@
+zoopassword=$2
+zookeeperurl=$3
+
+apt install -y openjdk-11-jdk 
+useradd zookeeper -m
+usermod --shell /bin/bash zookeeper 
+
+echo zookeeper:$zoopassword | chpasswd
+
+usermod -aG sudo zookeeper
+
+mkdir -p /opt/zookeeper/data
+chown -R zookeeper:zookeeper /opt/zookeeper
+
+
+cd /opt/
+wget $zookeeperurl
+
+tar xvzf apache-zookeeper*.tar.gz
+ 
+mv apache-zookeeper*bin apache-zookeeper
+
+chown -R zookeeper:zookeeper /opt/apache-zookeeper
+
+tee /opt/apache-zookeeper/conf/zoo.cfg << EOF
+tickTime = 2000
+dataDir = /opt/zookeeper/data
+clientPort = 2181
+initLimit = 5
+syncLimit = 2
+EOF
+
+chown -R zookeeper:zookeeper /opt/apache-zookeeper
+
+cd /opt/apache-zookeeper
+bin/zkServer.sh start
