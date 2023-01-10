971bfde8ea2df0f5fa06701120335b438be7a349
Alistair Henderson <al@alistairhenderson.com>
2023-01-10 07:41:21 +0000

    Update install-drill-service.sh

--- install-drill-service.sh
+++ install-drill-service.sh
@@ -5,14 +5,16 @@ Documentation=https://drill.apache.org/
 Requires=network.target
 After=network.target
 
-[Service]    
+[Service]
 Type=forking
-WorkingDirectory=/opt/drill
+WorkingDirectory=/opt/drill/
+
 User=root
 Group=root
-ExecStart=/opt/drill/ibin/drillbit.sh start --config /opt/drill-site/drill-override.conf
-ExecStop=/opt/drill/ibin/drillbit.sh stop --config /opt/drill-site/drill-override.conf
-ExecReload=/opt/drill/ibin/drillbit.sh restart --config /opt/drill-site/drill-override.conf
+
+ExecStart=/opt/drill/bin/drillbit.sh  --config /opt/drill-site/ start
+ExecStop=/opt/drill/bin/drillbit.sh  --config /opt/drill-site/ stop
+ExecReload=/opt/drill/bin/drillbit.sh  --config /opt/drill-site/ restart
 TimeoutSec=30
 Restart=on-failure
 [Install]
